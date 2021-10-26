59bca058aeda92ed914dd1ab545f12890c56ed7b
yasar2385 <yasar.mohideen@newgen.co>
2021-10-26 10:50:46 +0530

    1 file modified

--- index.js
+++ index.js
@@ -26,3 +26,13 @@ let frag = document.createRange().createContextualFragment(liens.join(''));
 console.log(liens.join(''));
 console.log(frag);
 document.getElementById('aaaapp').append(frag);
+const BIB_ID = (id) => {  
+  let sub = -Math.abs(id.toString().length);  
+  return 'CIT0000'.slice(0, sub)+id;
+};
+console.log(BIB_ID('9'));
+console.log(BIB_ID(10));
+console.log(BIB_ID(100));
+console.log(-Math.abs('100'));
+
+console.log('String'.slice(0, -2));
